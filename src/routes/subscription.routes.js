f3099b1a122a8cd8ebb7889f27581de35b27e338
Tanish Singla <131950694+tanishsingla51@users.noreply.github.com>
2025-08-31 13:40:52 +0530

    Add src/routes/subscription.routes.js

---
+++ src/routes/subscription.routes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import {
+  getSubscribedChannels,
+  getUserChannelSubscribers,
+  toggleSubscription,
+} from '../controllers/subscription.controller.js';
+import { verifyJWT } from '../middlewares/auth.middleware.js';
+
+const router = Router();
+router.use(verifyJWT);
+
+router.route('/c/:channelId').post(toggleSubscription);
+router.route('/u/:subscriberId').get(getSubscribedChannels);
+router.route('/c/:channelId').get(getUserChannelSubscribers);
+
+export default router;
